169ca2225f3bea8fbfdf9ba675b3fef5a746b7ef
richardatf-oss <richardatf@gmail.com>
2026-01-06 18:36:13 -0700

    Create lib.ts

---
+++ src/lib.ts
@@ -0,0 +1,7 @@
+export { tokenize } from "./tokenizer.js";
+export { assemble } from "./assembler.js";
+export { run } from "./vm.js";
+
+export type { Token } from "./tokenizer.js";
+export type { Instr } from "./assembler.js";
+export type { VMOptions } from "./vm.js";
